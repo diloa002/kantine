ae7838e54bb1012e6c3a8706b115a525f03f01eb
diloa002 <144429475+diloa002@users.noreply.github.com>
2024-05-22 10:38:29 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logg inn før du kan gå videre</title>
+        <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <?php
+include "nav.inc.php";
+
+    ?>
+<button><a href="login_ansatt.php">Logg inn som ansatt</a></button>
+<button><a href="login_kantine.php">Logg inn som kantinearbeider</a></button>
+
+<h3>Eller registrer ny bruker <a href="nybruker.php">HER</a></h3>
+
+</body>
+</html>
